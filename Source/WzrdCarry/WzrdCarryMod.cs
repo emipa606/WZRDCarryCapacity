f867fe1740d227bb3d7ab340b7b94bebf9b2c559
emipa606 <emil.offermo@litet.net>
2021-07-15 13:46:35 +0200

    Mod updated for 1.3 and passed autotests

--- Source/WzrdCarry/WzrdCarryMod.cs
+++ Source/WzrdCarry/WzrdCarryMod.cs
@@ -6,44 +6,45 @@ using Verse;
 
 namespace WzrdCarry
 {
-	// Token: 0x02000002 RID: 2
-	public class WzrdCarryMod : Mod
-	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-		public WzrdCarryMod(ModContentPack content) : base(content)
-		{
-			Log.Message("[WzrdCarry] - Init @ " + DateTime.Now.ToString(), false);
+    // Token: 0x02000002 RID: 2
+    public class WzrdCarryMod : Mod
+    {
+        // Token: 0x04000001 RID: 1
+        public static WzrdCarrySettings settings;
+
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public WzrdCarryMod(ModContentPack content) : base(content)
+        {
+            Log.Message("[WzrdCarry] - Init @ " + DateTime.Now);
             settings = GetSettings<WzrdCarrySettings>();
-			LongEventHandler.QueueLongEvent(delegate()
-			{
-				Go();
-			}, "WzrdCarry_Init", false, null);
-		}
+            LongEventHandler.QueueLongEvent(Go, "WzrdCarry_Init", false, null);
+        }
 
-		// Token: 0x06000002 RID: 2 RVA: 0x000020A8 File Offset: 0x000002A8
-		public override string SettingsCategory()
-		{
-			return "[WZRD] Carry Capacity";
-		}
+        // Token: 0x06000002 RID: 2 RVA: 0x000020A8 File Offset: 0x000002A8
+        public override string SettingsCategory()
+        {
+            return "[WZRD] Carry Capacity";
+        }
 
-		// Token: 0x06000003 RID: 3 RVA: 0x000020BF File Offset: 0x000002BF
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
+        // Token: 0x06000003 RID: 3 RVA: 0x000020BF File Offset: 0x000002BF
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
             settings.Draw(inRect);
-			Go();
-		}
+            Go();
+        }
 
-		// Token: 0x06000004 RID: 4 RVA: 0x000020D8 File Offset: 0x000002D8
-		private void Go()
-		{
-			ThingDef named = DefDatabase<ThingDef>.GetNamed("TransportPod", true);
-			CompProperties_Transporter compProperties_Transporter = named.comps.Single((CompProperties properties) => properties is CompProperties_Transporter) as CompProperties_Transporter;
-			compProperties_Transporter.massCapacity = settings.transportPodCapacity;
-			StatDef named2 = DefDatabase<StatDef>.GetNamed("CarryingCapacity", true);
-			named2.defaultBaseValue = settings.carryCapacity;
-		}
+        // Token: 0x06000004 RID: 4 RVA: 0x000020D8 File Offset: 0x000002D8
+        private void Go()
+        {
+            var named = DefDatabase<ThingDef>.GetNamed("TransportPod");
+            if (named.comps.Single(properties =>
+                properties is CompProperties_Transporter) is CompProperties_Transporter compProperties_Transporter)
+            {
+                compProperties_Transporter.massCapacity = settings.transportPodCapacity;
+            }
 
-		// Token: 0x04000001 RID: 1
-		public static WzrdCarrySettings settings;
-	}
+            var named2 = DefDatabase<StatDef>.GetNamed("CarryingCapacity");
+            named2.defaultBaseValue = settings.carryCapacity;
+        }
+    }
 }
